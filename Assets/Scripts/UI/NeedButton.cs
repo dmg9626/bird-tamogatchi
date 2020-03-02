6312b1ac2699fb16468e9983bef83bc3056afa8c
Drew Graham <dmg9626@gmail.com>
2020-03-02 16:33:10 -0500

    buttons now directly increment need values

---
+++ Assets/Scripts/UI/NeedButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class NeedButton : MonoBehaviour
+{
+    public Need need;
+
+    private Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!TryGetComponent(out button))
+            Debug.LogError("Missing button");
+
+        button.onClick.AddListener(() =>
+        {
+            need.Increase(.2f);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
