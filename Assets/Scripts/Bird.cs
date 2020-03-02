0f582a8ef4d4173e0c532e887e11bf4a056c11bf
Drew Graham <dmg9626@gmail.com>
2020-03-02 16:04:49 -0500

    restructured Bird script - moved stat stuff into separate BirdStatus script/object

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,64 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Bird : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
 
-    [SerializeField]
-    public Need hunger;
 
-    [SerializeField]
-    public Need fun;
-
-    [SerializeField]
-    public Need love;
-    
-    [SerializeField]
-    private Sprite unhappySprite;
-    
-    [SerializeField]
-    private Sprite neutralSprite;
-    
-    [SerializeField]
-    private Sprite happySprite;
-    
-    [SerializeField]
-    private Sprite maxHappySprite;
-
-    private Dictionary<SatisfactionLevel, Sprite> satisfactionSpriteDict;
-
-    private SatisfactionLevel satisfactionLevel;
-
+    // Use this for initialization
     void Start()
     {
-        satisfactionSpriteDict = new Dictionary<SatisfactionLevel, Sprite> {
-            { SatisfactionLevel.LOW, unhappySprite },
-            { SatisfactionLevel.MEDIUM, neutralSprite },
-            { SatisfactionLevel.HIGH, happySprite },
-            { SatisfactionLevel.MAX, maxHappySprite },
-        };
-
         if (!TryGetComponent(out spriteRenderer))
             Debug.LogError(name + " | missing SpriteRenderer component");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if(satisfactionLevel != love.satisfactionLevel) {
-            satisfactionLevel = love.satisfactionLevel;
-            UpdateSprite();
-        }
+
     }
 
-    void UpdateSprite()
-    {
-        if(!satisfactionSpriteDict.TryGetValue(satisfactionLevel, out Sprite sprite)) {
-            Debug.LogError(name + " | Bird.UpdateSprite() failed to pull sprite from dictionary");
-            return;
-        }
-        spriteRenderer.sprite = sprite;
-    }
+    
 }
---
+++ Assets/Scripts/BirdStatus.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdStatus : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    public Need hunger;
+
+    [SerializeField]
+    public Need fun;
+
+    [SerializeField]
+    public Need love;
+    
+    [SerializeField]
+    private Sprite unhappySprite;
+    
+    [SerializeField]
+    private Sprite neutralSprite;
+    
+    [SerializeField]
+    private Sprite happySprite;
+    
+    [SerializeField]
+    private Sprite maxHappySprite;
+
+    private Dictionary<SatisfactionLevel, Sprite> satisfactionSpriteDict;
+
+    private SatisfactionLevel satisfactionLevel;
+
+    void Start()
+    {
+        satisfactionSpriteDict = new Dictionary<SatisfactionLevel, Sprite> {
+            { SatisfactionLevel.LOW, unhappySprite },
+            { SatisfactionLevel.MEDIUM, neutralSprite },
+            { SatisfactionLevel.HIGH, happySprite },
+            { SatisfactionLevel.MAX, maxHappySprite },
+        };
+    }
+
+    void Update()
+    {
+        // If bird's Love level changes, update its sprite accordingly
+        if(satisfactionLevel != love.satisfactionLevel) {
+            satisfactionLevel = love.satisfactionLevel;
+            UpdateSprite();
+        }
+    }
+
+    void UpdateSprite()
+    {
+        if(!satisfactionSpriteDict.TryGetValue(satisfactionLevel, out Sprite sprite)) {
+            Debug.LogError(name + " | Bird.UpdateSprite() failed to pull sprite from dictionary");
+            return;
+        }
+    }
+}
