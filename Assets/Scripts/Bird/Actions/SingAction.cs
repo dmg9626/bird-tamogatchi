9d3fe5dcc1a7b56036cc542251a01711d4f8c1d5
Drew Graham <dmg9626@gmail.com>
2020-03-04 00:06:33 -0500

    added singing action for adult bird (same as chirp but different animation)

---
+++ Assets/Scripts/Bird/Actions/SingAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingAction : Action
+{
+    Animator animator;
+    private void Awake()
+    {
+        type = Type.SING;
+
+        if (!TryGetComponent(out animator))
+            Debug.LogError("SingAction | missing Animator component");
+    }
+
+    protected override IEnumerator ActionCoroutine()
+    {
+        // Trigger animation and play sound effect
+        animator.SetTrigger("Sing");
+        SoundController.Instance.PlaySoundEffect(SoundType.CHIRP);
+
+        // Wait a few seconds before marking coroutine as finished
+        yield return new WaitForSeconds(2f);
+        state = State.FINISHED;
+    }
+}
